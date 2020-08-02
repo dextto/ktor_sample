eb5c6ae26d508723aa2da3b66ee2f1c887809d56
Yongjae Han <dexter.haan@gmail.com>
2020-08-02 20:25:39 +0900

    application.conf에 정의된 대로 동작하도록 변경

--- src/Application.kt
+++ src/Application.kt
@@ -7,7 +7,9 @@ import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module(testing: Boolean = false) {
     val server = embeddedServer(Netty, 8080) {
         routing {
             get("/") {
@@ -17,20 +19,3 @@ fun main(args: Array<String>) {
     }
     server.start(wait = true)
 }
-
-//
-//import io.ktor.application.*
-//import io.ktor.response.*
-//import io.ktor.request.*
-//import io.ktor.client.*
-//
-//fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
-//
-//@Suppress("unused") // Referenced in application.conf
-//@kotlin.jvm.JvmOverloads
-//fun Application.module(testing: Boolean = false) {
-//    val client = HttpClient() {
-//    }
-//
-//}
-//
